0fe92dd61074e5d30a69977a74507d314a32d374
Mohamed Aslam <113659435+mdaslam32@users.noreply.github.com>
2024-06-27 21:21:50 +0530

    Update file.js

--- files/file.js
+++ files/file.js
@@ -1,30 +1,32 @@
 //! Importing required modules
-const fs = require("fs");
+const fsPromises = require("fs").promises;
 const path = require("path");
 
-//! Read a file using readFile()
-fs.readFile(path.join(__dirname, "readFile.txt"), "utf-8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
-
-//! Write a new file using writeFile()
-fs.writeFile(
-  path.join(__dirname, "writeFile.txt"),
-  "New file were created and writen by writeFile()",
-  (err) => {
-    console.log("Writing a new file is completed");
-
-    //! Edit an existing file using appendFile()
-    fs.appendFile(
-      path.join(__dirname, "writeFile.txt"),
-      "\n\nThis line were appended by appendFile()",
-      (err) => {
-        console.log("Appending a text of existing file were updated");
-      }
+const fileOpp = async () => {
+  try {
+    //! Read a file using readFile()
+    const data = await fsPromises.readFile(
+      path.join(__dirname, "readFile.txt"),
+      "utf-8"
     );
+    console.log(data);
+    //! Write a new file using writeFile()
+    await fsPromises.writeFile(
+      path.join(__dirname, "writeFile.txt"),
+      "New file were created and writen by writeFile()"
+    );
+    console.log("Writing a new file is completed");
+    //! Edit an existing file using appendFile()
+    await fsPromises.appendFile(
+      path.join(__dirname, "writeFile.txt"),
+      "\n\nThis line were appended by appendFile()"
+    );
+    console.log("Appending a text of existing file were updated");
+  } catch (err) {
+    console.log(err);
   }
-);
+};
+fileOpp();
 //! Capturing the uncaughtException error using process.on()
 process.on("uncaughtException", (err) => {
   console.error(`There was an uncaught ${err}`);
